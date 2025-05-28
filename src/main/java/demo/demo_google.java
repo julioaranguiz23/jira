a114b9039f89cb2cd04ec972948bfc516ee4920d
julioaranguiz23 <julio.aranguiz@testgroup.cl>
2025-05-28 11:05:07 -0400

    Update demo_google.java

--- src/main/java/demo/demo_google.java
+++ src/main/java/demo/demo_google.java
@@ -8,17 +8,19 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class demo_google {
     public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
-
+        
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
         String titulo = driver.getTitle();
